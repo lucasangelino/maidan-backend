977131afed5b857eb52e3d6967f604df96531e3a
Lucas Angelino <angelino.n.lucas@gmail.com>
2021-09-06 18:14:11 -0300

    MAI-BCK add .env

--- index.js.js
+++ index.js.js
@@ -1,12 +1,14 @@
+require("./mongo");
 const express = require("express");
 const cors = require("cors");
+
+// Schemas
+const Article = require("./models/article");
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
-
-const PORT = 5000;
 
 app.get("/", (req, res) => {
   res.json("Hello World!");
@@ -14,7 +16,13 @@ app.get("/", (req, res) => {
 
 app.get("/api/article/:id", (req, res) => {
   const id = Number(req.params.id);
-  res.json(`Article ${id}`);
+  Article.find({}, (err, data) => {
+    if (err) {
+      res.json(err);
+    } else {
+      res.json(data);
+    }
+  });
 });
 
 app.get("/api/categories", (req, res) => {
